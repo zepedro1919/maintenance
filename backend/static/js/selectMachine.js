930171ce95f66d52a2c6151e53ec569b76136855
zepedro1919 <130983842+zepedro1919@users.noreply.github.com>
2024-12-11 15:36:57 +0000

    Delete backend/static directory

--- backend/static/js/selectMachine.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.machine-button').forEach(button => {
-    button.addEventListener('click', () => {
-        const machineName = button.dataset.machine;
-        sessionStorage.setItem('selected_machine', machineName); // Store the machine name for later use
-        // Redirect to the next page (replace with your desired page)
-        window.location.href = '../templates/selectManutencao.html'
-    })
-})
--- backend/static/js/selectManutencao.js
+++
@@ -1,9 +0,0 @@
-const buttons = document.querySelectorAll('.maintenance-button');
-
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    const maintenanceType = button.dataset.type;
-    sessionStorage.setItem('maintenance_type', maintenanceType); // Store maintenance type
-    window.location.href = '../templates/index.html';
-  });
-});
